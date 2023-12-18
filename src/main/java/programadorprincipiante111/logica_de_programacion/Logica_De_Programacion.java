7cb81d358f369c40bba4e872960d06f0c44fbea0
Gabrie Andres <gabrielrubioduque22@gmail.com>
2023-12-18 13:58:37 -0500

    Ejercicios de logica programacion java con interfaz en swing

---
+++ src/main/java/programadorprincipiante111/logica_de_programacion/Logica_De_Programacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package programadorprincipiante111.logica_de_programacion;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Logica_De_Programacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
